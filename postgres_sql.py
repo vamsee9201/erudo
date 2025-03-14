cd2a7ad9129b7af1764882bbf3e8f26376fcee8a
vamsee9201 <vamseekrishna9201@gmail.com>
2025-03-13 23:30:49 -0500

    tried something new

--- postgres_sql.py
+++ postgres_sql.py
@@ -3,3 +3,9 @@
 #setup connection with postgres
 
 #Write sample queries with postgres. 
+
+
+#Need to figure out a way to connect to postgres from python. 
+
+
+#Need to first establish a postgres database on my local machine. 
