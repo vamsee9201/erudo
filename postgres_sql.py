a7b285cf1411ccd5a7eca631a98dbf1f98b0a2c4
vamsee9201 <vamseekrishna9201@gmail.com>
2025-03-24 23:39:25 -0500

    added postgres code

--- postgres_sql.py
+++ postgres_sql.py
@@ -1,11 +1,62 @@
+# Note: the module name is psycopg, not psycopg3
 #%%
+import psycopg
+#%%
+var = ""
+# Connect to an existing database
+with psycopg.connect("dbname=postgres user=postgres password=Maxverstappen@33") as conn:
 
-#setup connection with postgres
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
 
-#Write sample queries with postgres. 
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE test (
+                id serial PRIMARY KEY,
+                num integer,
+                data text)
+            """)
+
+        # Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no SQL injections!)
+        cur.execute(
+            "INSERT INTO test (num, data) VALUES (%s, %s)",
+            (100, "abc'def"))
+
+        # Query the database and obtain data as Python objects.
+        cur.execute("SELECT * FROM test")
+        cur.fetchone()
+        # will return (1, 100, "abc'def")
+
+        # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
+        # of several records, or even iterate on the cursor
+        for record in cur:
+            var = record
+            print(record)
+
+        # Make the changes to the database persistent
+        conn.commit()
+# %%
+import psycopg2
+db_name = "postgres"
+db_user = "postgres"
+db_password = "mysecretpassword"
+db_host = "localhost"  # or your database host
+db_port = "5433" # default port if not specified
 
 
-#Need to figure out a way to connect to postgres from python. 
+conn = psycopg2.connect(
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port
+    )
 
-
-#Need to first establish a postgres database on my local machine. 
+# %%
+cur = conn.cursor()
+cur.execute("SELECT * FROM table_name")
+# %%
+for record in cur:
+    print(record)
+#%%
