d87b496928a9d9f7986898cc0624b6cce8b0f4ec
vamsee9201 <vamseekrishna9201@gmail.com>
2025-03-29 22:14:02 -0500

    added key

--- erudo_streamlit/qa.py
+++ erudo_streamlit/qa.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import json
+import os
 
 """
 {"users":{"user_id":"a","name":"b","email":""},
@@ -7,7 +9,7 @@ import streamlit as st
 
 """
 
-def qa_functionality(tables_data):
+def get_db_description(tables_data):
     st.header("Ask a Question")
     question = st.text_input("Type your question here:")
     
@@ -25,6 +27,43 @@ def qa_functionality(tables_data):
     
         # print("----Tables Data----")
     return explanation_text   # st.write(tables_data) 
+
+with open('openai_key.json', 'r') as file:
+    data = json.load(file)
+os.environ['OPENAI_API_KEY'] = data["api_key"]
+
+from typing_extensions import TypedDict
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+
+
+from typing_extensions import Annotated
+
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+explanation_text = ""
+
+
+from langchain.chat_models import init_chat_model
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+def write_query(state: State):
+    """Generate SQL query to fetch information."""
+    # Updated prompt to include explanation text and database details
+    prompt = (
+        f"Please write a PostgreSQL query based on the following question: '{state['question']}'. "
+        f"Ensure that the query does not include any DML statements. "
+        f"Here is the structure of the tables and their columns:\n\n{explanation_text}"
+    )
+    structured_llm = llm.with_structured_output(QueryOutput)
+    result = structured_llm.invoke(prompt)
+    return {"query": result["query"]}
 
 
 
