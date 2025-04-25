a3832f08d07d18f56181fe6e23c24e8de08a99cf
vamsee9201 <vamseekrishna9201@gmail.com>
2025-04-24 22:00:27 -0500

    trying to create a frontend

---
+++ erudo-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <input type="text" placeholder="Input 1" />
+      <input type="text" placeholder="Input 2" />
+      <input type="text" placeholder="Input 3" />
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App
